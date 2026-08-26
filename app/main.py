6bc9824f9abd2d4608fc90a9cb6330fbc5ade075
prashant260 <prashantagchoudhary2004@gmail.com>
2026-08-26 20:59:34 +0530

    final commit

--- app/main.py
+++ app/main.py
@@ -33,6 +33,7 @@ def info():
         "environment": "development",
         "message": "Feature branch deployment"
     })
-    
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
